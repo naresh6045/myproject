0c171ff389859620bcc6707a6af2294d3423b8f9
naresh6045 <nareshkotla@gmail.com>
2025-02-18 05:53:34 -0500

    Update script.sh for jenkins git integration

--- script.sh
+++ script.sh
@@ -1,11 +1,2 @@
 date
 echo "naresh"
-this is key based auth
-branch2 1st changes
-branch2 2nd changes
-branch3 3rd changes
-main branch cn]hanges
-naresh1 changes again
-navi changes to see merge conflict
-fetch changes
-fectch more changes to test
